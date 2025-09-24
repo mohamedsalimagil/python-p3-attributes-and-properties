8baea298d498c48e17679fc62580e6df2301209b
Mohamed Salim Agil <mohamedsalimagil.dev@gmail.com>
2025-09-24 21:56:21 +0300

    python-p3-attributes-and-properties

--- lib/dog.py
+++ lib/dog.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-APPROVED_BREEDS = [
+#!/usr/bin/env python3
+
+approved_breeds = [
     "Mastiff",
     "Chihuahua",
     "Corgi",
@@ -12,4 +14,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Fido", breed=None):
+        self.name = name
+        self.breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value is None or value in approved_breeds:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,32 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="John Doe", job=None):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str):
+            v = value.strip()
+            if 1 <= len(v) <= 25:
+                self._name = v.title()
+                return
+        print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value is None:
+            self._job = None
+        elif value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
